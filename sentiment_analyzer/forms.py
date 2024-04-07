0de1cdd778f56eb1e4c0a2c85a7d7a406e12fe6b
Gowtham G <119653141+imgowthamg@users.noreply.github.com>
2024-04-07 11:02:29 +0530

    Update forms.py

--- sentiment_analyzer/forms.py
+++ sentiment_analyzer/forms.py
@@ -1,5 +1,7 @@
 # sentiment_analyzer/forms.py
+
 from django import forms
 
 class UserInputForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
+    language = forms.ChoiceField(choices=[('en', 'English'), ('fr', 'French'), ('es', 'Spanish')])  # Add more languages as needed
